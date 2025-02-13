0e9322b9a2200df660503f904d7f40ccf3a10b78
Chris Lawlor <chris.d.lawlor@gmail.com>
2025-02-13 14:10:41 +0000

    added Sentry

---
+++ week-10-hackathon-beards-n-babs/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://56a64f32c840b6a409378bbdfdc97c79@o4508800256114688.ingest.de.sentry.io/4508812241141840",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
